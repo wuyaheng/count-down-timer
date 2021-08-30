94989a16e866ce0cba69d3e212c903500e13a352
wuyaheng <wuyaheng2016@gmail.com>
2021-08-29 21:43:11 -0400

    pause and resume

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var startButton = document.getElementById("start");
+var actionButton = document.getElementById("action");
 var resetButton = document.getElementById("reset");
 var stopWatch = document.getElementById("stopWatch");
 var timeSelect = document.getElementById("timeSelect");
@@ -7,6 +7,7 @@ var circle = document.getElementById("circle");
 var counter = 5;
 var startTime = 5;
 var interval = null;
+var isPaused = false;
 
 timeSelect.addEventListener("change", function(e) {
   startTime = e.target.value;
@@ -15,20 +16,14 @@ timeSelect.addEventListener("change", function(e) {
   }
 });
 
-startButton.addEventListener("click", function() {
+actionButton.addEventListener("click", function() {
   if(interval==null) {
-    counter = startTime;
-    interval = setInterval(function(){
-      counter--;
-      if (counter <= 0) {
-        stopWatch.textContent = "Time's Up!";
-        clearInterval(interval)
-        interval=null;
-      } else {
-        stopWatch.textContent = parseTime(counter);
-      } 
-    }, 1000)
-  } 
+    startTimer();
+  } else if (isPaused) {
+    resumeTimer()
+  } else {
+    pauseTimer()
+  }
 });
 
 function parseTime(counter) {
@@ -38,8 +33,40 @@ function parseTime(counter) {
 
 
 resetButton.addEventListener("click", function() {
+  actionButton.innerHTML="<i class='fas fa-play'></i> Start ";
   counter = startTime;
   clearInterval(interval)
   interval=null;
+  isPaused = false;
   stopWatch.textContent = parseTime(counter);
 });
+
+
+function startTimer() {
+  actionButton.innerHTML="<i class='fas fa-pause'></i> Pause ";
+  counter = startTime;
+  interval = setInterval(function(){
+    if(!isPaused) {
+      counter--;
+      if (counter <= 0) {
+        stopWatch.textContent = "Time's Up!";
+        clearInterval(interval)
+        interval=null;
+      } else {
+        stopWatch.textContent = parseTime(counter);
+      }
+    } 
+  }, 1000)
+}
+
+
+function pauseTimer() {
+  actionButton.innerHTML="<i class='fas fa-play'></i> Resume ";
+  isPaused = true;
+}
+
+
+function resumeTimer() {
+  actionButton.innerHTML="<i class='fas fa-pause'></i> Pause ";
+  isPaused = false;
+}
